5d1094a6ba81d8106528abe04377ac6cbc0e23e9
Nicole Casartelli <44043709+NicCas@users.noreply.github.com>
2020-12-04 20:30:07 -0800

    Subtree search
    Added implementation of subtree search from BinaryTree

--- Main.java
+++ Main.java
@@ -9,6 +9,44 @@ public class Main {
 
         System.out.println();
         System.out.println("---------- Question 3 ----------");
+        
+         BinaryTree compareTree = new BinaryTree();
+
+        compareTree.rootContainer = new Node (100);
+        compareTree.rootContainer.l = new Node (60);
+        compareTree.rootContainer.l.r = new Node (50);
+        compareTree.rootContainer.l.l = new Node (40);
+        compareTree.rootContainer.l.l.r = new Node (30);
+        compareTree.rootContainer.l.l.l = new Node (20);
+        compareTree.rootContainer.l.l.l.r = new Node (3);
+        compareTree.rootContainer.l.l.l.l = new Node (11);
+        compareTree.rootContainer.r = new Node (70);
+        compareTree.rootContainer.r.r = new Node (2);
+        compareTree.rootContainer.r.l = new Node (8);
+
+        compareTree.rootTarget = new Node (20);
+        compareTree.rootTarget.l = new Node (11);
+        compareTree.rootTarget.r = new Node (3);
+
+        if (compareTree.isSubtree(compareTree.rootContainer, compareTree.rootTarget))
+            System.out.println("Subtree");
+        else
+            System.out.println("Not a Subtree");
+
+        compareTree.rootContainer = new Node (100);
+        compareTree.rootContainer.l = new Node (60);
+        compareTree.rootContainer.l.r = new Node (50);
+        compareTree.rootContainer.l.l = new Node (40);
+
+        compareTree.rootTarget = new Node (60);
+        compareTree.rootTarget.l = new Node (50);
+        compareTree.rootTarget.r = new Node (3);
+
+        if (compareTree.isSubtree(compareTree.rootContainer, compareTree.rootTarget))
+            System.out.println("Subtree");
+        else
+            System.out.println("Not a Subtree");
+
     }
 
     public static int questionTwo(int[] array)
